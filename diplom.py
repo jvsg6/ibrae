d36a221475d1e1d7ceaa2dc20718d4f8fa8b9f00
jvsg6 <jvsg6@mail.ru>
2017-12-03 15:14:34 +0300

    new ver

--- diplom.py
+++ diplom.py
@@ -22,14 +22,14 @@ def f2(y, t):
 	a=2
 	b=3
 	c=4
-	s=2*(a*b+a*c+b*c)
+	s=a*b+a*c+b*c+a*b+a*c+b*c #b*c - пол
 	v=a*b*c
 	T=300
 	R=8.31
 	BAS4=4*10**(-3)
 	BAS62=5*10**(-9)
 	BAS76=1*10**(-5)
-	
+	print s, v, float(s)/float(v)
 	EAKT4=0
 	EAKT62=8.22*10**4
 	EAKT76=0
@@ -42,12 +42,12 @@ def f2(y, t):
 	
 	
 	y1, y2, y3 = y
-	return [-k4*y1*s/v+k62*s/1000.0/v*y2, k4*y1*1000.0-k62*y2-k76*y2+k77*y3*0.5, k76*y2-k77*y3]
+	return [-k4*y1*s/v+k62*s/v*y2, k4*y1-k62*y2-k76*y2+k77*y3*0.5, k76*y2-k77*y3]
 
 def main():
 	fig = plt.figure(facecolor='white')
 	y0 = [1.0, 0.0]
-	t = np.linspace(0.0,500.0,31.0) # вектор моментов времени
+	t = np.linspace(0.0,4000.0,500.0) # вектор моментов времени
 	[y1,y2]=odeint(f1, y0, t, full_output=False).T
 	plt.plot(t,y1, linewidth=2) # график решения x(t) слева
 	plt.plot(t,y2, linewidth=2) # фазовая траектория справа
@@ -57,6 +57,7 @@ def main():
 	#y = np.array(y).flatten() # преобразование массива
 	#plt.plot(t, y,'-sr',linewidth=3) # построение графика
 	fig2=plt.figure(facecolor='white')
+	t = np.linspace(0.0,40000.0,500.0)
 	y0 = [1.0, 0.0, 0.0]
 	[y1,y2, y3]=odeint(f2, y0, t, full_output=False).T
 	plt.plot(t,y1, linewidth=2) # график решения x(t) слева
