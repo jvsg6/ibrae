de9c7fdf3ca073c9354bd0c02e6ee301d51c65af
jvsg6 <jvsg6@mail.ru>
2018-05-30 10:39:25 +0300

    add db read

--- DoseModels/calcCommEff_food_pre.py
+++ DoseModels/calcCommEff_food_pre.py
@@ -4,7 +4,7 @@ import sys
 import os
 from math import exp
 
-
+import sqlite3
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from scipy.integrate import quad
@@ -34,9 +34,9 @@ dictI131 = {    "e_pl_srf": 3.8e-16,
 		"e_inh_ad" : 7.4e-09,
 		"e_ing_inf" : 1.8e-07,
 		"e_ing_ad"  : 2.2e-08,
-		"delta_eff": 6.4e+05,
-		"Fcons"    : 9.2e-01,
-		"Tfeed_cow_milk" : 4.7e+02,
+		"del_eff_OLI_3": 6.4e+05,
+		"F_cons"    : 9.2e-01,
+		"T_feed_cow_milk" : 4.7e+02,
 		}	
 dictCs134 = {    "e_pl_srf": 1.5e-15,
 		"l"       : 1.0e-06,
@@ -44,9 +44,9 @@ dictCs134 = {    "e_pl_srf": 1.5e-15,
 		"e_inh_ad" : 7.4e-09,
 		"e_ing_inf" : 1.6e-08,
 		"e_ing_ad"  : 1.9e-08,
-		"delta_eff": 1.7e+06,
-		"Fcons"    : 1.0e+00,
-		"Tfeed_cow_milk" : 4.0e+02,
+		"del_eff_OLI_3": 1.7e+06,
+		"F_cons"    : 1.0e+00,
+		"T_feed_cow_milk" : 4.0e+02,
 		}	
 dictCs136 = {    "e_pl_srf": 1.5e-15,
 		"l"       : 1.0e-06,
@@ -54,9 +54,9 @@ dictCs136 = {    "e_pl_srf": 1.5e-15,
 		"e_inh_ad" : 7.4e-09,
 		"e_ing_inf" : 9.5e-09,
 		"e_ing_ad"  : 3.0e-09,
-		"delta_eff": 8.5e+05,
-		"Fcons"    : 9.5e-01,
-		"Tfeed_cow_milk" : 4.0e+02,
+		"del_eff_OLI_3": 8.5e+05,
+		"F_cons"    : 9.5e-01,
+		"T_feed_cow_milk" : 4.0e+02,
 		}
 dictCs137 = {    "e_pl_srf": 1.5e-15,
 		"l"       : 1.0e-06,
@@ -64,34 +64,34 @@ dictCs137 = {    "e_pl_srf": 1.5e-15,
 		"e_inh_ad" : 7.4e-09,
 		"e_ing_inf" : 1.2e-08,
 		"e_ing_ad"  : 1.3e-08,
-		"delta_eff": 1.7e+06,
-		"Fcons"    : 1.0e+00,
-		"Tfeed_cow_milk" : 4.0e+02,
+		"del_eff_OLI_3": 1.7e+06,
+		"F_cons"    : 1.0e+00,
+		"T_feed_cow_milk" : 4.0e+02,
 		}
 
 def calc_e_ing_pre(dictNucl):
-	f1 = 3
-	Fmilk = 0.5
-	Ucow = 1.9e-04
-	Fcow_feed = 0.7
-	Tfeed_cow_milk = dictNucl["Tfeed_cow_milk"]
+	f1 = 3.0 #
+	Fmilk = 0.5 #
+	Ucow = 1.851852E-04 #
+	Fcow_feed = 0.7 #
+	Tfeed_cow_milk = dictNucl["T_feed_cow_milk"] 
 	
-	Qmilk_inf = 3.8e-06
-	eing_inf = dictNucl["e_ing_inf"]
-	Qmilk_ad = 3.3e-06
-	eing_ad = dictNucl["e_ing_ad"]
+	Qmilk_inf = 3.805175E-06 #
+	eing_inf = dictNucl["e_ing_inf"] 
+	Qmilk_ad = 3.329528E-06 #
+	eing_ad = dictNucl["e_ing_ad"] 
 	max_milk = max(Qmilk_inf*eing_inf, Qmilk_ad*eing_ad)
 	
-	Qlv_inf = 6.3e-07
-	eing_inf = dictNucl["e_ing_inf"]
-	Qlv_ad = 1.9e-06
-	eing_ad = dictNucl["e_ing_ad"]
+	Qlv_inf = 6.341958E-07 #
+	eing_inf = dictNucl["e_ing_inf"] 
+	Qlv_ad = 1.902588E-06 #
+	eing_ad = dictNucl["e_ing_ad"] 
 	max_lv = max(Qlv_inf*eing_inf, Qlv_ad*eing_ad)
 	
-	f2 = 0.3
+	f2 = 0.3 
 	Flv = 0.5
-	delta_eff = dictNucl["delta_eff"]
-	Fcons = dictNucl["Fcons"]
+	delta_eff = dictNucl["del_eff_OLI_3"] #
+	Fcons = dictNucl["F_cons"] #
 	e_ing_pre = (f1*Fmilk*Ucow*Fcow_feed*Tfeed_cow_milk*max_milk+max_lv*f2*Flv)*delta_eff*Fcons
 	#print "time", t
 	return e_ing_pre
@@ -106,7 +106,18 @@ def createGraph(x, y, name, log):
 	save(name)
 	plt.close(fig)
 	return
-	
+
+
+dictI131 = {    "e_pl_srf": 3.8e-16,
+		"l"       : 1.0e-06,
+		"e_air_sh_ad" : 1.8e-14,
+		"e_inh_ad" : 7.4e-09,
+		"e_ing_inf" : 1.8e-07,
+		"e_ing_ad"  : 2.2e-08,
+		"del_eff_OLI_3": 6.4e+05,
+		"F_cons"    : 9.2e-01,
+		"T_feed_cow_milk" : 4.7e+02, #
+		}	
 
 def main():
 	print 
@@ -117,6 +128,37 @@ def main():
 	print calc_e_ing_pre(dictCs136)
 	print
 	print calc_e_ing_pre(dictCs137)
+	
+	dictNuclide = {}
+	print "--------------------------------------------"
+	#Устанавливаем соединение с базой данных
+	conn = sqlite3.connect('./db/new_db.sqlite')
+	
+	# Создаем курсор - это специальный объект который делает запросы и получает их результаты
+	cursor = conn.cursor()
+		
+	for i in range(1,6):
+		cursor.execute("SELECT T_feed_cow_milk FROM Table_21_Transfer_factor_from_feed_to_milk WHERE id={0}".format(i))
+		dictNuclide.update({"T_feed_cow_milk" : cursor.fetchall()[0][0]})
+		cursor.execute("SELECT e_ing_inf FROM Table_27_Dose_conv_for_Food_pre_and_post_analysis WHERE id={0}".format(i))
+		dictNuclide.update({"e_ing_inf" : cursor.fetchall()[0][0]})
+		cursor.execute("SELECT e_ing_ad FROM Table_27_Dose_conv_for_Food_pre_and_post_analysis WHERE id={0}".format(i))
+		dictNuclide.update({"e_ing_ad" : cursor.fetchall()[0][0]})
+		cursor.execute("SELECT del_eff_OLI_3 FROM Table_20_Effective_availability_period WHERE id={0}".format(i))
+		dictNuclide.update({"del_eff_OLI_3" : cursor.fetchall()[0][0]})
+		cursor.execute("SELECT F_cons FROM Table_22_Fraction_remaining_human_consumption WHERE id={0}".format(i))
+		dictNuclide.update({"F_cons" : cursor.fetchall()[0][0]})
+		cursor.execute("SELECT Decay_const FROM Table_6_Half_life_adn_decay_const WHERE id={0}".format(i))
+		dictNuclide.update({"l" : cursor.fetchall()[0][0]})
+		cursor.execute("SELECT nuclide FROM Table_24_Conversion_fraction_total_eff_dose_ground_scenario WHERE id={0}".format(i))
+		nuclide = cursor.fetchall()[0][0]
+		print 
+		print nuclide
+		print  
+		print calc_e_ing_pre(dictNuclide)
+		print "--------------------------------------------"
+	#Закрываем соединение с базой данных
+	conn.close()
 	#x = range(0,31536000, 6000)
 	#y = [calcEgrd(time) for time in x]
 	#createGraph(x, y, "FromDayToYearLog", True)
